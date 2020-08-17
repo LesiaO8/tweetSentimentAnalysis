75f9d2d147e9455c4ffcc43661aabbdfc9444e2f
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2020-08-17 15:03:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tweetSentimentAnalysis
+ This small program accesses the Twitter API using tweepy library to search for a keyword and gathers tweets in a list that contain the specified keyword. Then each tweet's sentiments are analyzed by using TextBlob and the average sentiment polarity is shown.
---
+++ sentimentAnalyzer.py
@@ -0,0 +1,45 @@
+import tweepy
+import os 
+from textblob import TextBlob
+
+#Authenticate to twitter
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+
+    listOfTweets = []
+
+    #search for tweet query 
+    for tweet in api.search(q="la croix", lang="en", rpp=10):
+        t = f"{tweet.text}"
+
+        if ":" in t:
+            colon = t.index(":")
+            t = t[colon + 1:]
+        listOfTweets.append(t)
+
+    num = 1
+    score = 0
+    count = len(listOfTweets)
+
+    for tweet in listOfTweets:
+        print(num, tweet)
+        num += 1
+        text = TextBlob(tweet)
+        print(text.sentiment.polarity)
+        score += text.sentiment.polarity
+        print("---------------------------")
+    print("Average Sentiment Polarity on current topic is:", score/count)
+
+except:
+    print("Error during authentication")
